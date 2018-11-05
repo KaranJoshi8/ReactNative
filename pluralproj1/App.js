2e4987421d6fd9711128948471a1c3c835a2f5c9
KaranJoshi8 <karan@jbspl.com>
2018-11-05 15:52:44 +0530

    Counter Program Done
    Counter Program as per plural sight

---
+++ pluralproj1/App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import EventList from './EventList';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <EventList/>
+    );
+  }
+}
+
